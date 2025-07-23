4af4118c64f3127ade0cbd8eacfd4533fc666b2e
Daniel Hermon <danielhermonsync@gmail.com>
2025-07-23 17:07:29 +0300

    build(deps)!: upgrade to latest packages (#43)
    Major change:
    1. Node engine break
    -------------------------
    
    * build!(deps): upgrade to latest packages
    
    * revert: adding back commitlint.config.js
    
    * refactor: changed Interface to type since it is equivelent to its supertype
    
    * build(deps): configured husky correctly
    
    * revert: no need for husky install in package
    
    * build(deps): added missing rimraf package
    
    ---------
    
    Co-authored-by: Daniel Hermon <danielhermonsync+github@gmail.com>

--- tests/configurations/unit/jest.config.js
+++ tests/configurations/unit/jest.config.js
@@ -1,6 +1,10 @@
+const { createDefaultPreset } = require('ts-jest');
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
 module.exports = {
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    ...tsJestTransformCfg,
   },
   globals: {
     'ts-jest': {
